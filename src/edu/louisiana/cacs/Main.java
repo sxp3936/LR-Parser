fe07ffce561166d40af4bf3efeef0595be18febe
sxp3936 <sxp3936@users.noreply.github.com>
2014-12-08 18:12:34 -0600

    Update Main.java

--- src/edu/louisiana/cacs/Main.java
+++ src/edu/louisiana/cacs/Main.java
@@ -1,17 +1,11 @@
 package edu.louisiana.cacs;
 
-import java.io.IOException;
-
 import edu.louisiana.cacs.csce450GProject.Parser;
 
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World from Main");
-        try {
-			Parser.parse("data/sample.txt");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        Parser myParser = new Parser("data/sample.txt");
+        myParser.parse();
     }
 }
